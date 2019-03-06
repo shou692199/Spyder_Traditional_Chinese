52263974331a32596c78a02d9c2aceadb1e47362
蕭澧邦 <45505768+shou692199@users.noreply.github.com>
2019-03-06 21:57:46 +0800

    Add files via upload

---
+++ pofile/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"If you run this pack in Administractor mode just "<<endl; 
+    system("pause");    
+    system("cls");
+   	cout<<"Loading ーーーーーーーーーー 0%";
+   	Sleep(500);
+	system("cls");
+   	cout<<"Loading ―ーーーーーーーーー 10%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ――ーーーーーーーー 20%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ―――ーーーーーーー 30%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ――――ーーーーーー 40%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ―――――ーーーーー 50%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ――――――ーーーー 60%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ―――――――ーーー 70%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ――――――――ーー 80%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ―――――――――ー 90%";
+   	Sleep(500);
+   	system("cls");
+   	cout<<"Loading ―――――――――― 100%"<<endl;
+   	cout<<"Done, for install ";
+   	system("pause");
+   	system("python setup.py");
+   	system("C:\ProgramData\Anaconda3\Scripts\spyder.exe");
+   	return 0;
+}
